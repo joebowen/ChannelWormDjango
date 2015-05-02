615b36961e0037ed6226a149b6f4667c5ae51cc3
Joe Bowen <joe.bowen@toptal.com>
2015-05-02 17:18:03 -0600

    First commit.

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^$', 'ChannelWorm.views.home', name='home'),
+    # url(r'^$', 'ChannelWorm.views.home', name='home'),
 )

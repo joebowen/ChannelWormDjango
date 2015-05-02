d950c636ac65ece502a14e7aaf294a47e15f2825
Joe Bowen <joe.bowen@toptal.com>
2015-05-02 07:29:01 -0600

    first commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ChannelWorm',
+      version='1.0',
+      description='Channel Worm Django Projecto',
+      author='OpenWorm',
+      author_email='example@example.com',
+      url='http://www.openworm.org/',
+#      install_requires=['Django>=1.3'],
+     )
